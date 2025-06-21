0c2c20c7b764e49b768136e7ee61cb27f0012a40
Mai Toan Thang <mth2610@gmail.com>
2025-06-21 17:31:59 +0700

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "336196971"
+    serviceWorkerVersion: "1401862958"
   }
 });
