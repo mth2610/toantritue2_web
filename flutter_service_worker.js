62bf958f20709850c1090fc920f8b5211499aff6
Mai Toan Thang <mth2610@gmail.com>
2025-08-26 17:38:02 +0700

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8ed380084c90e7bb6bc4ce5e7da2363c",
+const RESOURCES = {"flutter_bootstrap.js": "a5e6cdfe4156dbe2e9923fc371e88e52",
 "version.json": "19b524d4d487016a6a0e1dceffdd8b61",
 "index.html": "a93a8c3c5254524e4af8759c93788d72",
 "/": "a93a8c3c5254524e4af8759c93788d72",
-"main.dart.js": "6ecf15bf33963fe09bf74a30e4ccffaf",
+"main.dart.js": "c026fb8bc7177d1d2d935b1bbc1eaa71",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "78a107443ebbbb22242c558da7c237eb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
